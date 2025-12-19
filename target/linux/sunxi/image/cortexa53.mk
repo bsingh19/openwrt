ad6e1654c2908777e8b5954f8b14d48257fe20d4
Bhupinder Dylon <bhupinder926@hotmail.com>
2025-12-19 05:47:34 -0500

    add hello-world module and wpad-basic-mbedtls to Orange Pi Zero 3 device packages
    Signed-off-by: Bhupinder Dylon <bhupinder926@hotmail.com>

--- target/linux/sunxi/image/cortexa53.mk
+++ target/linux/sunxi/image/cortexa53.mk
@@ -130,7 +130,7 @@ TARGET_DEVICES += xunlong_orangepi-zero2
 define Device/xunlong_orangepi-zero3
   DEVICE_VENDOR := Xunlong
   DEVICE_MODEL := Orange Pi Zero 3
-  DEVICE_PACKAGES := kmod-uwe5622
+  DEVICE_PACKAGES := kmod-uwe5622 kmod-hello-world wpad-basic-mbedtls
   $(Device/sun50i-h618)
 endef
 TARGET_DEVICES += xunlong_orangepi-zero3
